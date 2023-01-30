fe9a6634e7c87a39e9365b7596571a8b9224479c
Nguyễn Nghị  Nhân <ng.nghinhan.dev@gmail.com>
2023-01-30 15:53:42 +0700

    add wish

---
+++ happy_new_year.js
@@ -0,0 +1,15 @@
+function hpny() {
+  myWish = () => {
+    document.getElementById("body").innerHTML = `
+    <div class="container" id="wish">
+      <h1>
+        Anh với em cùng nhau đón thêm một cái tết nữa, năm nay 2 đứa mình đã
+        cùng nhau trải qua rất nhiều, anh rất may mắn vì anh có một người iu
+        đồng hành dễ thương như vậy đi chung với anh, năm sau sẽ lại là một năm
+        rất dài, bé của anh hãy đi cùng anh tiếp nha bé, anh cảm ơn bé một năm
+        qua đã chịu đựng và chăm sóc anh, anh iu bé lắmmm
+      </h1>
+    </div>`;
+  };
+  setTimeout(myWish, 6000);
+}
